ed9dbd3fe40a8e3a7d0d7c79af3c76f33174f5bb
Tim <Timothy.MGregg@gmail.com>
2021-10-18 20:37:47 -0400

    transfer to laptop

--- home.js
+++ home.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-    $("p").click(function(){
-      $(this).hide();
-    });
-  });
+document.getElementById("addGame").addEventListener("click", addGame())
+
+function api_call(options) {
+  return fetch("api.formicid.io", options)
+}
+
+function post(body) {
+  return fetch({
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: body
+  }
+  )
+}
+
+function addGame() {
+  post("balls")
+}
