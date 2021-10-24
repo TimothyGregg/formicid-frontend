4d68fc2dfc99d90cad818dd7c901d3ad3f3339a0
Tim <timothy.mgregg@gmail.com>
2021-10-24 10:45:25 -0400

    Some updates

--- home.js
+++ home.js
@@ -1,11 +1,21 @@
-document.getElementById("addGame").addEventListener("click", addGame())
+//https://stackoverflow.com/questions/8739605/getelementbyid-returns-null
+
+// code that should be taken care of right away
+
+window.onload = init;
+
+function init(){
+  // the code to be called when the dom has loaded
+  // #document has its nodes
+  document.getElementById("addGame").addEventListener("click", addGame)
+}
 
 function api_call(options) {
-  return fetch("api.formicid.io", options)
+  fetch("api.formicid.io", options)
 }
 
 function post(body) {
-  return fetch({
+  api_call({
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/TimothyGregg/formicid/web/util"
 	"github.com/gorilla/mux"
 )
 
@@ -12,17 +13,20 @@ type Server struct {
 	http.Server
 }
 
-
-func New_Server() (*Server) {
+func New_Server() *Server {
 	s := &Server{}
 
 	// build router
 	router := mux.NewRouter().StrictSlash(true)
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./")))
+	router.Use(util.LogToStderr)
 	s.Handler = router
 
 	port := os.Getenv("PORT")
 	s.Addr = ":" + port
+
+	// debug
+	// fmt.Println(s.Addr)
 
 	return s
 }
