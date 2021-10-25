022d0051223010f143c7a2d95784fe668b210ff6
Tim <Timothy.MGregg@gmail.com>
2021-10-25 16:49:41 -0400

    web test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/TimothyGregg/formicid-frontend
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/TimothyGregg/formicid v0.0.0-20211024144540-f3d3f0e8d682
+	github.com/gorilla/mux v1.8.0
+)
--- home.js
+++ home.js
@@ -4,27 +4,33 @@
 
 window.onload = init;
 
+const endpoint = "api.formicid.io"
+
 function init(){
   // the code to be called when the dom has loaded
   // #document has its nodes
-  document.getElementById("addGame").addEventListener("click", addGame)
+  document.getElementById("addGame").addEventListener("click", addGame(100, 100))
 }
 
-function api_call(options) {
-  fetch("api.formicid.io", options)
-}
-
-function post(body) {
-  api_call({
+function post(relative_url, body) {
+  var options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
     body: body
   }
-  )
+  fetch(endpoint + relative_url, options)
 }
 
-function addGame() {
-  post("balls")
+function addGame(size_x, size_y) {
+  var addGameOptions = {
+    action_type: 1,
+    action_name: 'addGame',
+    details: {
+      size_x: size_x,
+      size_y: size_y
+    }
+  }
+  post("/", addGameOptions)
 }
